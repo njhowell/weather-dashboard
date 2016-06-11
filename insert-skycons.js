86e1ffde493962f3fe9491b758d139a87d431701
njhowell <nick@nickhowell.co.uk>
2016-06-11 22:22:23 +0100

    Add the rest of the animations

--- insert-skycons.js
+++ insert-skycons.js
@@ -3,26 +3,59 @@ var icons = new Skycons()
 var x = document.getElementsByName("clear-day");
 var i;
 for (i = 0; i < x.length; i++) {
-    icons.add(x[i], Skycons.CLEAR_DAY)
+    icons.add(x[i], Skycons.CLEAR_DAY);
+}
+
+var x = document.getElementsByName("clear-night");
+var i;
+for (i = 0; i < x.length; i++) {
+    icons.add(x[i], Skycons.CLEAR_NIGHT);
 }
 
 var x = document.getElementsByName("partly-cloudy-day");
 var i;
 for (i = 0; i < x.length; i++) {
-    icons.add(x[i], Skycons.PARTLY_CLOUDY_DAY)
+    icons.add(x[i], Skycons.PARTLY_CLOUDY_DAY);
 }
 
+var x = document.getElementsByName("partly-cloudy-night");
+var i;
+for (i = 0; i < x.length; i++) {
+    icons.add(x[i], Skycons.PARTLY_CLOUDY_NIGHT);
+}
+
+var x = document.getElementsByName("cloudy");
+var i;
+for (i = 0; i < x.length; i++) {
+    icons.add(x[i], Skycons.CLOUDY);
+}
 var x = document.getElementsByName("rain");
 var i;
 for (i = 0; i < x.length; i++) {
-    icons.add(x[i], Skycons.RAIN)
+    icons.add(x[i], Skycons.RAIN);
 }
 
+var x = document.getElementsByName("sleet");
+var i;
+for (i = 0; i < x.length; i++) {
+    icons.add(x[i], Skycons.SLEET);
+}
 var x = document.getElementsByName("snow");
 var i;
 for (i = 0; i < x.length; i++) {
-    icons.add(x[i], Skycons.SNOW)
+    icons.add(x[i], Skycons.SNOW);
 }
 
+var x = document.getElementsByName("wind");
+var i;
+for (i = 0; i < x.length; i++) {
+    icons.add(x[i], Skycons.WIND);
+}
+
+var x = document.getElementsByName("fog");
+var i;
+for (i = 0; i < x.length; i++) {
+    icons.add(x[i], Skycons.FOG);
+}
 
 icons.play();
