887be45641e950f6cece59cce99ee99f28c44de6
njhowell <nick@nickhowell.co.uk>
2016-06-12 13:41:37 +0100

    Pull data from forecast.io

--- weather-dashboard.py
+++ weather-dashboard.py
@@ -1,10 +1,25 @@
 from flask import Flask
 from flask import render_template
+import forecastio
+from datetime import datetime
+from dateutil import tz
+
+import config
 app = Flask(__name__)
 
+@app.template_filter('localtime')
+def _jinja2_filter_datetime(date):
+    return date.replace(tzinfo=tz.tzutc()).astimezone(tz.gettz('Europe/London')).strftime("%H%M")
+
+
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def index():
+    lat = 52.3454
+    lng = 0.3347
+
+    forecast = forecastio.load_forecast(config.forecastio_api_key, lat, lng)
+
+    return render_template('index.html', forecast=forecast)
 
 
 
